53a763c7b3846da6ed57ad0e5569f9610cedf0fd
EJHacks <ee1912136@gmail.com>
2025-11-17 13:18:45 -0500

    Update cli.py
    new update

--- bookbot/cli.py
+++ bookbot/cli.py
@@ -1,1 +1,11 @@
 #BookBot 
+
+
+def main():
+    with open("bookbot/books/frankenstein.txt") as f:
+        # do something with f (the file) here
+        file_contents = f.read()
+        print(file_contents)
+
+    
+    return file_contents
