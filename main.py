baa109684de27d70baed7f02839ef5436d8c32cc
EJ-Edwards <ee1912136@gmail.com>
2025-11-28 09:49:39 -0500

    Version1 status more to come soon

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ from stats import (
     get_chars_dict,
 )
 
+
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-
     book_path = sys.argv[1]
-    text = get_book_text(book_path)
 
+    text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-
     print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
-    with open(path, "r", encoding="utf-8") as f:
+    with open(path) as f:
         return f.read()
 
 
@@ -31,7 +30,6 @@ def print_report(book_path, num_words, chars_sorted_list):
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
@@ -40,5 +38,4 @@ def print_report(book_path, num_words, chars_sorted_list):
     print("============= END ===============")
 
 
-if __name__ == "__main__":
-    main()
+main()
