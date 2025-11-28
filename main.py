c31f9b241353fc7a7df48bdb29a2d553fd426d4f
EJ-Edwards <ee1912136@gmail.com>
2025-11-27 20:28:49 -0500

    a description of your changes here

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+from stats import count_words
+
+def get_book_text(filepath):
+    with open(filepath, "r") as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+
+    # Print first line (for BootDev test)
+    first_line = text.splitlines()[0]
+    print(first_line)
+
+    # Count words and print
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
+
+if __name__ == "__main__":
+    main()
